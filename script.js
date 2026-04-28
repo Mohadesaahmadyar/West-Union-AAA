bc1e906276f750b8002f0d9ddb2cffc1168a18af
Mohadesa <mohadesaahmadyar37@gmail.com>
2026-04-28 10:19:45 -0700

    Touching up the memberships section.

---
+++ script.js
@@ -0,0 +1,5 @@
+const joinBtn = document.getElementById("joinBtn");
+
+joinBtn.addEventListener("click", function () {
+  alert("Thank you for your interest! Please call us to start your membership.");
+});
