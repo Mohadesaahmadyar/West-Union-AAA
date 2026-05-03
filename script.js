199755400f3e1a15545b392db7539413d266aec2
Mohadesa <mohadesaahmadyar37@gmail.com>
2026-05-03 11:38:42 -0700

    starting the next sectioms

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const joinBtn = document.getElementById("joinBtn");
 joinBtn.addEventListener("click", function () {
   alert("Thank you for your interest! Please call us to start your membership.");
 });
+
+const giftBtn = document.querySelector(".gift__btn");
+
+giftBtn.addEventListener("click", function () {
+  alert("Gift membership feature coming soon!");
+});
+
